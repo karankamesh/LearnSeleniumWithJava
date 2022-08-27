41a7a08b6809c68955fa65f062afd531f81a322e
Kamesh <101127152+karankamesh@users.noreply.github.com>
2022-08-27 13:41:49 +0530

    Add files via upload
    This project shows how to handle Text Boxes in the Webpage.

---
+++ Basics_4_WorkingWithTextBox_1.java
@@ -0,0 +1,44 @@
+package learnSeleniumWithJava;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Basics_4_WorkingWithTextBox_1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver", "F:\\Selenium Tools\\Firefox Driver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.leafground.com/input.xhtml");
+		
+		//In this project we will find element with their name.
+		
+		driver.findElement(By.name("j_idt88:name")).sendKeys("Karan");
+		
+		driver.findElement(By.name("j_idt88:j_idt91")).sendKeys(" Example");
+		
+		boolean disabled = driver.findElement(By.name("j_idt88:j_idt93")).isEnabled();
+		System.out.println("The Text Box is Enabled :"+disabled);
+		
+		driver.findElement(By.name("j_idt88:j_idt95")).clear();
+		
+		String text =  driver.findElement(By.name("j_idt88:j_idt97")).getAttribute("value");
+		System.out.println("The Text is : "+text);
+		
+        driver.findElement(By.name("j_idt88:j_idt99")).sendKeys("example@gmail.com"+Keys.TAB);
+        WebElement control = driver.switchTo().activeElement();
+        String value = control.getAttribute("placeholder");
+		if (value.equals("About yourself")) {
+			System.out.println("The control is Moved");
+			control.sendKeys("My Name Is Karan");
+		}else {
+			System.out.println("The control is Not Moved");
+		}
+		
+		
+
+	}
+
+}
